330cb2a50c8cba8776e4110c70a7bc06c51ce6b5
KaiReptilon <aaon.edwards2@hotmail.com>
2017-04-30 15:29:38 +0100

    Created EmployeeData class with simple data such as names and shift start/end times

---
+++ src/main/java/com/aaronedwards/ci346/EmployeeData.java
@@ -0,0 +1,62 @@
+package com.aaronedwards.ci346;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by aaron on 30/04/17.
+ */
+@Data
+@Entity
+public class Employees {
+
+    private @Id @GeneratedValue Long EmployeeId;
+
+    private String EmployeeFirstName;
+    private String EmployeeSurname;
+    private String ShiftStartTime;
+    private String ShiftFinishTime;
+    private String ShiftType;
+
+    private Employees() {}
+
+    public Employees( String firstName, String Surname, String Start, String Finish, String Type){
+
+        this.EmployeeFirstName = firstName;
+        this.EmployeeSurname = Surname;
+        this.ShiftStartTime = Start;
+        this.ShiftFinishTime = Finish;
+        this.ShiftType = Type;
+
+    }
+
+
+    public String getFirstName(){
+        return this.EmployeeFirstName;
+    }
+
+    public String getSurname() {
+        return this.EmployeeSurname;
+    }
+
+    public String getShiftStartTime() {
+        return this.ShiftStartTime;
+    }
+
+    public String getShiftFinishTime() {
+        return this.ShiftFinishTime;
+    }
+
+    public String getShiftType() {
+        return this.ShiftType;
+    }
+}
+
+
+
+
+
+
